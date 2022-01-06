9f8aa1a548907103a99310a4ef361227cd56ce04
Arnaud Delcroix <arnaud.delcroix@FR-BES-DKT14098.dynamic.besancon.parkeon.com>
2022-01-06 15:02:39 +0100

    Add some languages and some translations

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import { useTranslation, withTranslation } from 'react-i18next';
 function MyComponent() {
 
   const {t, i18n} = useTranslation();
+  
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   }
 
   return (
       <div className='traduction'>
-        <div className='traductionInputs'>
-        <button className='traductionInput' type="button" onClick={() => changeLanguage('question')}>Reset</button>
-        <button className='traductionInput' type="button" onClick={() => changeLanguage('fr')}>Français</button>
-        <button className='traductionInput' type="button" onClick={() => changeLanguage('en')}>English</button>
+      <h1 className='traductionTitle'>{t("fact")}</h1>
+        <div className='traductionInputs'>    
+          <button className='traductionInput' type="button" onClick={() => changeLanguage('fr')}>
+            {t("language1")}
+          </button>
+          <button className='traductionInput' type="button" onClick={() => changeLanguage('en')}>
+            {t("language2")}
+          </button>
+          <button className='traductionInput' type="button" onClick={() => changeLanguage('pl')}>
+            {t("language3")}
+          </button>
+          <button className='traductionInput' type="button" onClick={() => changeLanguage('es')}>
+            {t("language4")}
+          </button>
+          <button className='traductionInput' type="button" onClick={() => changeLanguage('sw')}>
+            {t("language5")}
+          </button>
         </div>
           
+        <p className='traductionText'>{t("Welcome")}</p>
 
-        <p>{t("Welcome")}</p>
+        <button className='traductionInput' type="button" onClick={() => changeLanguage('question')}>Reset</button>
       </div>
   );
 }
